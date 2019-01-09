7077630f7e0cb91d32d13b9707caffd115e68759
n3tsky <n3tsky@protonmail.com>
2019-01-09 22:59:36 +0100

    Handle arguments (argparse)

--- pythPwned.py
+++ pythPwned.py
@@ -1,63 +1,175 @@
 #!/usr/bin/env python
 # Inspired by https://github.com/thewhiteh4t/pwnedOrNot
-import argparse
+import sys
+from argparse import ArgumentParser, RawTextHelpFormatter
 import requests
-import cfscrape
+#import cfscrape
 import json
+from time import sleep
+from tabulate import tabulate
 
 # Constant
-BASE_URL="https://haveibeenpwned.com/api/v2/breachedaccount"
+BASE_URL="https://haveibeenpwned.com/api/v2"
 USER_AGENT="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0"
+DEFAULT_WAIT=2
+# Color
+ORANGE="\033[0;33m"
+NOCOLOR="\033[0m"
 
-def make_query():
+# Perform HTTP query
+def make_query(cf, HTTP_REQ):
     # Request through cfscrape (Cloud-flare)
-    if cfscrape:
-        return cfscrape.get_tokens(HTTP_REQ["url"], user_agent=HTTP_REQ["user-agent"])
+    if cf:
+        return None
+        #return cfscrape.get_tokens(HTTP_REQ["url"], user_agent=HTTP_REQ["user-agent"])
     else: # Normal request
-        proxy = {} # {'http': "socks://127.0.0.1:8181"}
         headers = {
             "User-Agent": "%s" % (HTTP_REQ["user-agent"]),
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept_language": "en-US,en;q=0.5",
             "Accept_encoding": "gzip, deflate, br"
         }
-        return requests.get(HTTP_REQ["url"], headers=headers, proxy=HTTP_REQ["proxy"])
+        return requests.get(HTTP_REQ["url"], headers=headers, proxies=HTTP_REQ["proxy"])
 
-def http_query(csfscrape, HTTP_REQ):
+def http_query(cf, HTTP_REQ):
     try:
-        return make_query(csfscrape, HTTP_REQ)
+        return make_query(cf, HTTP_REQ)
     except requests.exceptions.HTTPError as e:
-        print "[!] Exception while performing HTTP request"
+        print "[!] Exception while performing HTTP request to %s" % (HTTP_REQ["url"])
         return None
+
+# Display data to user (grid)
+def display_data(data, headers):
+    print(tabulate(data, headers, "grid"))
+
+# Check if value is in given dictionary or return None
+def check_and_get(d, value):
+    return d[value] if value in d else None
+
+# Parse JSON result from HIBP
+def parse_json(data, needed):
+    g = list()
+    for d in data:
+        l = list()
+        for n in needed:
+            l.append(check_and_get(d, n))
+        g.append(l)
+    return g
 
 # Have I Been Pwned related
 def hibp_get_cookies():
     HTTP_REQ = {}
     HTTP_REQ["url"] = "%s/%s" % (BASE_URL, "test@test.com")
     HTTP_REQ["user-agent"] = USER_AGENT
-    return http_query(1, HTTP_REQ)
+    #return http_query(1, HTTP_REQ)
 
-def hibp_query_breach():
-    HTTP_REQ = {}
-    HTTP_REQ["url"] = "%s/%s" % (BASE_URL, "contact@contact.com")
-    HTTP_REQ["user-agent"] = USER_AGENT
-    HTTP_REQ["proxy"] = ""
-    req = http_query(1, HTTP_REQ)
+# Query HIBP
+def hibp_query(HTTP_REQ, append_url, user_account):
+    HTTP_REQ["url"] = "%s/%s" % (BASE_URL, append_url)
+    req = http_query(0, HTTP_REQ)
 
-    print req.status_code
     if req.status_code == 200:
-        print "OK"
+        # Parse results
+        try:
+            return json.loads(req.text)
+        except ValueError as e:
+            print e
+    elif req.status_code == 429:
+        print "[!]  HTTP 429 - Too many requests. Sleeping a bit..."
+        #time.sleep(1)
+    elif req.status_code == 404:
+        print "[-]  No data (HTTP 404)"
     else:
-        print "error"
-        print req.text
+        print "[!]  HTTP error (code: %s)" % (req.status_code)
+
+    return None
+
+# Query Have I Been Powned for breached accounts
+def hibp_query_breach(HTTP_REQ, user_account):
+    print "[*] Looking for account in leak(s)"
+    json_data = hibp_query(HTTP_REQ, "breachedaccount/%s" % (user_account), "")
+    if json_data != None:
+        NEEDED = ["Title", "BreachDate", "Domain", "DataClasses","IsVerified", "IsFabricated", "IsSensitive", "IsRetired", "IsRetired"]
+        GRID_HEADERS = ["Title", "Date", "Domain", "Leaked data", "Verified", "Fabricated", "Sensitive", "Retired", "Spam"]
+
+        GRID_DATA = parse_json(json_data, NEEDED)
+        ## Refining
+        # Order by date
+        GRID_DATA.sort(key=lambda r: r[1], reverse=True)
+        for g in GRID_DATA:
+            g[3] = ", ".join(g[3])
+
+        # Display
+        display_data(GRID_DATA, GRID_HEADERS)
+
+# Query Have I Been Powned for pasted accounts
+def hibp_query_paste(HTTP_REQ, user_account):
+    print "[*] Looking for account in paste(s)"
+    json_data = hibp_query(HTTP_REQ, "pasteaccount/%s" % (user_account), "")
+    if json_data != None:
+        NEEDED = ["Source", "Id", "Title", "Date"]
+        GRID_DATA = parse_json(json_data, NEEDED)
+        ## Refining
+        # Order by date
+        GRID_DATA.sort(key=lambda r: r[3], reverse=True)
+        for g in GRID_DATA:
+            if g[0] == "Pastebin":
+                g[1] = "https://www.pastebin.com/raw/%s" % (g[1])
+        display_data(GRID_DATA, NEEDED)
+
+def read_file_2_list(filename):
+    try:
+        with open(filename, "r") as fin:
+            return [l.rstrip() for l in fin.readlines()]
+    except IOError as e:
+        print "[!] Cannot open file \"%s\" (%s).\nExiting..." % (filename, e)
+        sys.exit(1)
 
 # Main
-def main():
-    cookies = hibp_get_cookies()
+def main(args):
+    # Set up HTTP_REQ
+    HTTP_REQ = {}
+    HTTP_REQ["user-agent"] = args.user
+    HTTP_REQ["proxy"] = {} if (len(args.proxy) == 0) else {"https": args.proxy, "http": args.proxy}
+    HTTP_REQ["time"] = args.t
+
+    print "[-] Starting %s" % (sys.argv[0])
+    print "[*]   User-Agent: %s" % (HTTP_REQ["user-agent"])
+    print "[*]   Proxy: %s" % (HTTP_REQ["proxy"])
+    print "[*]   Time: %ss.\n" % (HTTP_REQ["time"])
+
+    # if user provided a file
+    if args.f:
+        emails = read_file_2_list(args.f)
+    else:
+        emails = args.e
+    print "[*] Loaded %d email(s)\n" % (len(emails))
+
+    # Loop through email(s)
+    for email in emails:
+        #cookies = hibp_get_cookies()
+        print "\n[*] Results for \"%s%s%s\"\n" % (ORANGE, email, NOCOLOR)
+        hibp_query_breach(HTTP_REQ, email)
+        #sleep(args.t)
+        hibp_query_paste(HTTP_REQ, email)
+        #sleep(args.t)
+
 
 # Parse arguments
 def parse_args():
-    parser = argparse.ArgumentParser(description="")
+    parser = ArgumentParser(description="Python utility to query HaveIBeenPwned API", formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-e", required=False, metavar="email", help="Email to test for leakage/paste")
+    parser.add_argument("-f", required=False, metavar="file", help="File with emails to test for leakage/paste")
+    parser.add_argument("-t", required=False, metavar="time", default=DEFAULT_WAIT, type=int, help="Time to wait between requests (default: %ss.)" % DEFAULT_WAIT)
+    parser.add_argument("--user", required=False, metavar="User-agent", default=USER_AGENT, help="Change default user-agent (default: %s)" % USER_AGENT)
+    parser.add_argument("--proxy", required=False, metavar="proxy", default="", help="Proxy to perform HTTP requests (ie.: http://localhost:8080, socks://localhost:8080)")
+    args = parser.parse_args()
+   
+    if (not args.e) and (not args.f):
+        print "[!] %s requires either a single email (-e) or file with multiple emails (-m)\nExiting..." % (sys.argv[0])
+        sys.exit(1)
+    return args
 
 if __name__ == "__main__":
-    query_breach()
+    args = parse_args()
+    main(args)
